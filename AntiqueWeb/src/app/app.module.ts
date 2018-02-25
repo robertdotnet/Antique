1b5d9c2278507d384d34cc3438b1dfe4d1ac3a2e
Robert Munteanu <munteanurobert96@yahoo.com>
2018-02-25 20:19:14 +0200

    music page(not routed) + reworked home component integration

--- AntiqueWeb/src/app/app.module.ts
+++ AntiqueWeb/src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MusicComponent } from './music/music.component';
 
 
+const appRoutes:Routes=[
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'music',
+    component:MusicComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    MusicComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forChild([
+      {path:'music', component:MusicComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
- 
